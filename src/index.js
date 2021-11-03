f616efb41fea41fa7a018d578a027e4f7f7755d7
pandeyrakesh835 <92678116+pandeyrakesh835@users.noreply.github.com>
2021-11-03 22:28:47 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import reactDom from "react-dom";
+import App from './App'
+import Topbar from "./Componants/Topbar";
+import Chart from './Componants/Chart/Chart'
+
+
+reactDom.render( <App/>, document.getElementById('root'))
